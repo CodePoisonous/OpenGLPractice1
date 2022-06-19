85d94df041c1c3802e2b14124f8df841734ec4e6
chuyan1 <379482455@qq.com>
2022-06-19 22:25:55 +0800

    动态修改矩形颜色的r值

--- src/Application.cpp
+++ src/Application.cpp
@@ -115,11 +115,11 @@ int main(void)
 {
 	GLFWwindow* window;
 
-	/* Initialize the library */
+	// 初始化glfw
 	if (!glfwInit())
 		return -1;
 
-	/* Create a windowed mode window and its OpenGL context */
+	// 创建窗口模式窗口及其 OpenGL 上下文
 	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
 	if (!window)
 	{
@@ -127,14 +127,20 @@ int main(void)
 		return -1;
 	}
 
-	/* Make the window's context current */
+	// 使window成为当前的上下文
 	glfwMakeContextCurrent(window);
 
+	// 设置刷新率为60帧
+	glfwSwapInterval(1);
+
+	// 判断glew初始化是否成功
 	if (glewInit() != GLEW_OK)
 		std::cout << "Error!" << std::endl;
-
+	
+	// 获取gl版本号
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
+	// 图形端点位置xy坐标
 	float positions[] = {
 		-0.5f, -0.5f,	// 0
 		 0.5f, -0.5f,	// 1
@@ -142,11 +148,13 @@ int main(void)
 		-0.5f,  0.5f,	// 3 
 	};
 
+	// 图形端点的索引序号
 	unsigned int indices[] = {
 		0, 1, 2,
 		2, 3, 0
 	};
 
+	// 生成positions的buffer
 	unsigned int buffer;
 	GLCall(glGenBuffers(1, &buffer));
 	GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
@@ -155,28 +163,44 @@ int main(void)
 	GLCall(glEnableVertexAttribArray(0));
 	GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));
 
+	// 生成indices的buffer
 	unsigned int ibo;			// index buffer object
 	GLCall(glGenBuffers(1, &ibo));
 	GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
 	GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW));
 
-
+	// 读取shader文件，生成shader program
 	ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
 	unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
-	glUseProgram(shader);
+	GLCall(glUseProgram(shader));
 
-	/* Loop until the user closes the window */
+	// 设置fragment shader 的颜色
+	GLCall(int location = glGetUniformLocation(shader, "u_Color"));
+	ASSERT(location != -1);
+	GLCall(glUniform4f(location, 0.8f, 0.3f, 0.8f, 1.0f));
+
+	float r = 0.0f;
+	float increment = 0.05f;
+	// 循环直到用户关闭窗口
 	while (!glfwWindowShouldClose(window))
 	{
-		/* Render here */
-		glClear(GL_COLOR_BUFFER_BIT);
+		// 渲染
+		GLCall(glClear(GL_COLOR_BUFFER_BIT));
 
-		GLCall(glDrawElements(GL_TRIANGLES, 6, GL_INT, nullptr));
+		GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
+		GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
 
-		/* Swap front and back buffers */
+		if (r > 1.0f)
+			increment = -0.05f;
+		else if (r < 0.0f)
+			increment = 0.05f;
+
+		r += increment;
+
+		// 交换前后缓冲区
 		glfwSwapBuffers(window);
 
-		/* Poll for and process events */
+		// 轮询和处理事件
 		glfwPollEvents();
 	}
 
